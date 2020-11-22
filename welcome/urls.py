afd1492c04590c754f7c26c93b04f7c995cd371b
Greeva Khant <Gravyhi5@gmail.com>
2020-11-22 18:41:35 +0530

    Not complete

--- welcome/urls.py
+++ welcome/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from welcome import views
 import sell_staff.views as vws
+import Check_Stock.views as cvws
 # SET THE NAMESPACE!
 app_name = 'welcome'
 # Be careful setting the name to just /login use userlogin instead!
@@ -8,4 +9,5 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='user_login'),
     url(r'^sell/$', vws.sell, name='sell'),
+    url(r'^check_stock/$', cvws.check_stock, name='check_stock'),
 ]
