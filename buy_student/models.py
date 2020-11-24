834820fe28f1751c8dca53a2d7042d48a1f7aed5
ishika <ishikaishu2000@gmail.com>
2020-11-24 14:18:51 +0530

    buy done

--- buy_student/models.py
+++ buy_student/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from sell_staff import models as smd
-from welcome import models as wmd
 
 # Create your models here.
 
@@ -12,3 +10,13 @@ class Cart(models.Model):
     Item_quantity = models.IntegerField(max_length=2, default=1)
     email_id = models.EmailField(default="")
     Order_id = models.IntegerField(primary_key=True, default=111)
+
+
+class Order(models.Model):
+    Item_id = models.IntegerField(default=0)
+    Item_title = models.CharField(max_length=100)
+    Item_price = models.FloatField()
+    Item_quantity = models.IntegerField(max_length=2, default=1)
+    email_id = models.EmailField(default="")
+    Order_id = models.IntegerField(default=111)
+    Order_date = models.DateField(default='2000-10-05')
--- buy_student/views.py
+++ buy_student/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from sell_staff.models import Items
-from  buy_student.models import Cart
+from buy_student.models import Cart, Order
 import random
-from welcome.models import Users
+from datetime import date
 # Create your views here.
 
 
@@ -40,7 +40,73 @@ def display_all_details(request):
 
 
 def view_cart(request):
+    which = request.GET.get('which')
+    if which != '2':
+        which = 1
+    else:
+        which = 2
+    print(which)
     f = open('loggedin.txt', 'r')
     email = f.readline()
     i = Cart.objects.filter(email_id=email)
-    return render(request, 'buy_student/view_cart.html', {'items': i})
+    if which == 1 and i != Cart.objects.none():
+        return render(request, 'buy_student/view_cart.html', {'items': i})
+    elif which == 2 and i != Cart.objects.none():
+        return render(request, 'buy_student/remove_item.html', {'items': i})
+    else:
+        return render(request, 'buy_student/view_cart.html', {'message': 'No items in the cart!'})
+
+
+def remove_an_item(request):
+    if request.method == 'POST':
+        order_id = request.POST.get('order_id')
+        f = open('loggedin.txt', 'r')
+        email = f.readline()
+        try:
+            c = Cart.objects.get(email_id=email, Order_id=order_id)
+            c.delete()
+            i = Cart.objects.filter(email_id=email)
+            return render(request, 'buy_student/view_cart.html', {'items': i, 'message': 'The item has been removed successfully'})
+        except Cart.DoesNotExist:
+            i = Cart.objects.filter(email_id=email)
+            return render(request, 'buy_student/remove_item.html', {'items': i, 'message': 'Kindly type the order id of an item in cart'})
+
+
+def empty_cart(request):
+    f = open('loggedin.txt', 'r')
+    email = f.readline()
+    c = Cart.objects.filter(email_id=email)
+    c.delete()
+    return render(request, 'buy_student/view_cart.html', {'message': 'No items in the cart!'})
+
+
+def goto_empty_confirm(request):
+    return render(request, 'buy_student/empty_confirm.html')
+
+
+def goto_checkout(request):
+    return render(request, 'buy_student/checkout.html')
+
+
+def generate_invoice(request):
+    f = open('loggedin.txt', 'r')
+    email = f.readline()
+    c = Cart.objects.filter(email_id=email)
+    oid = random.randint(100, 999)
+    for item in c:
+        o = Order(Item_id=item.Item_id, Item_title=item.Item_title,
+                  Item_price=item.Item_price, Item_quantity=item.Item_quantity,
+                  email_id=email, Order_id=oid, Order_date=date.today().strftime("%Y-%m-%d"))
+        o.save()
+    c.delete()
+    c = Order.objects.filter(email_id=email)
+    return render(request, 'buy_student/display_orders.html', {'items': c, 'message': 'Order has been successfully placed'})
+
+
+def goto_display_order(request):
+    f = open('loggedin.txt', 'r')
+    email = f.readline()
+    c = Order.objects.filter(email_id=email)
+    if c == Order.objects.none():
+        return render(request, 'buy_student/display_orders.html', {'message': 'No orders placed yet'})
+    return render(request, 'buy_student/display_orders.html', {'items': c})
